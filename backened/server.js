aa9b67ab90c0b802a90bc6e8bcffbd4aa89c1f62
Soojal Bajaj Jain <145982728+Soojal2005@users.noreply.github.com>
2025-06-16 11:23:41 -0400

    Update server.js

--- backened/server.js
+++ backened/server.js
@@ -1,6 +1,6 @@
 const app = require('./src/app');
 require('dotenv').config();
-const URL = ' https://render.com/docs/web-services#port-binding';
+const URL = ' https://render.com/docs/web-services#port-binding/ai/ask-ai';
 app.listen(URL, () => {
 console.log('Server is running on port 3131');
 });
